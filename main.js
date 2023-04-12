872fb25c92f9c53481ee3cb6e725c469326da320
Balasubramanian1999 <absmani1999@gmail.com>
2023-04-12 11:31:56 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,13 @@
+var parentLi=document.querySelector('.parent');
+var dropdown=document.querySelector('.dropdown');
+
+console.log(dropdown);
+parentLi.addEventListener('click', function(){
+	// if(dropdown.style.display.contains("none")){
+		dropdown.style.display="inline-block";
+	// }
+	// else{ 
+		// dropdown.style.display="none";
+	// }
+	
+})
